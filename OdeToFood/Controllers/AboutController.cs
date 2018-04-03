b6935c304a4968a08fc0f1c22b46cc693e85532f
tombeau19 <thomas.beauregard@gmail.com>
2018-04-03 14:21:39 -0400

    added attribute routes

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace OdeToFood.Controllers
 {
+    //about
+    [Route("about")]
     public class AboutController
     {
+        [Route("")]
         public string Phone()
         {
             return "339-206-5563";
         }
+
+        [Route("address")]
         public string Address()
         {
             return "USA!";
