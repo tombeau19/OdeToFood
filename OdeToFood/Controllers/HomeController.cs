b7946a1cf5619608a37c7472aaa21738bfef140f
tombeau19 <thomas.beauregard@gmail.com>
2018-04-03 16:19:04 -0400

    added ViewModels to consolidate information sent to views

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -3,22 +3,28 @@ using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
 using OdeToFood.Services;
 using Microsoft.AspNetCore.Mvc.Razor;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,
+                             IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
 
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = _greeter.GetMessageOfTheDay();
 
             return View(model);
         }
---
+++ OdeToFood/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Models;
+
+namespace OdeToFood.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public string CurrentMessage { get; set; }
+    }
+}
